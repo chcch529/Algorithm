a111e50e1b942ae55c574ecd3e0ab109fe99e1c4
chaerin <146617430+chaechae529@users.noreply.github.com>
2025-01-08 15:37:59 +0900

    [Bronze IV] Title: No Brainer, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i =0; i<T;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            String result = a<b? "NO BRAINS" : "MMM BRAINS";
+
+            System.out.println(result);
+        }
+
+    }
+}
