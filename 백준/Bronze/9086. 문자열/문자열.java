76b22da0f77fb63127d99e3099e1156d9be14a6c
chaerin <146617430+chaechae529@users.noreply.github.com>
2025-01-09 14:00:54 +0900

    [Bronze V] Title: 문자열, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,7 +12,7 @@ public class Main {
             String str = br.readLine();
             System.out.printf("%c%c%n",str.charAt(0), str.charAt(str.length()-1));
         }
-
+        br.close();
 
     }
 }
