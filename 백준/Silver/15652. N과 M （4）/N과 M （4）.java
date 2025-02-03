cba43180ae1e1d43d311ac1ea785cfb4d5abab54
chaerin <146617430+chaechae529@users.noreply.github.com>
2025-02-03 18:02:49 +0900

    [Silver III] Title: N과 M (4), Time: 144 ms, Memory: 17048 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    static StringBuilder sb= new StringBuilder();
+    static int[] arr;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        dfs(N,M,0, 1);
+
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int n, int m, int cnt, int idx){
+        if (cnt == m){
+            for (int i: arr){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+
+
+            return;
+        }
+
+        for(int i=idx; i<=n; i++){
+
+            arr[cnt] = i;
+            dfs(n,m, cnt+1, i);
+
+
+        }
+    }
+
+}
