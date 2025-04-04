c5e132dad7e5708a880bbf5febfa627e3e715405
chaechae529 <146617430+chaechae529@users.noreply.github.com>
2025-04-04 14:41:38 +0900

    [Bronze V] Title: 꼬마 정민, Time: 108 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        System.out.println(a+b+c);
+
+    }
+}
+
