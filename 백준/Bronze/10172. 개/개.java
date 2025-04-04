8710833e2b0054c015b2dd47108168a39644f64a
chaechae529 <146617430+chaechae529@users.noreply.github.com>
2025-04-04 14:35:40 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+//        int n = Integer.parseInt(st.nextToken());
+        String sb = "|\\_/|\n"
+                + "|q p|   /}\n"
+                + "( 0 )\"\"\"\\\n"
+                + "|\"^\"`    |\n"
+                + "||_/=\\\\__|\n";
+
+        System.out.println(sb);
+
+    }
+}
+
